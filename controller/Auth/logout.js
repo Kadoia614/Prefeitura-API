82942ec5e73a3ac7cabddecdacb4a2023f3b0696
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-02-19 10:29:05 -0300

    ajsutes rotas de logout

--- controller/Auth/logout.js
+++ controller/Auth/logout.js
@@ -1,9 +1,12 @@
 exports.logout = async (request, reply) => {
   try {
-    reply.clearCookie("token");
-  reply.status(200).send("Logout bem-sucedido");
+    reply.clearCookie("token", {
+      httpOnly: false, // Deve ser consistente com a configuração do cookie
+      secure: false, // Deve ser consistente com a configuração do cookie
+      sameSite: "none", // Deve ser consistente com a configuração do cookie
+    });
+    reply.status(200).send("Logout bem-sucedido");
   } catch (error) {
-    reply.status(error.status || 500).send(error.messagem || 'erro interno no servidor')
+    reply.status(error.status || 500).send(error.message || 'erro interno no servidor');
   }
-  
 };
